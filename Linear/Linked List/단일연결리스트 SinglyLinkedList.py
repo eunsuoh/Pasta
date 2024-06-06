66e99e3b782fbb1ac972563969b2022ab5e2a0cd
Eunsu <sublimage1109@gmail.com>
2024-06-06 14:58:19 +0900

    Create 단일연결리스트 SinglyLinkedList.py

---
+++ Linear/Linked List/단일연결리스트 SinglyLinkedList.py
@@ -0,0 +1,31 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+
+    def append(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+            return
+        last_node = self.head
+        while last_node.next:
+            last_node = last_node.next
+        last_node.next = new_node
+
+    def print_list(self):
+        cur_node = self.head
+        while cur_node:
+            print(cur_node.data)
+            cur_node = cur_node.next
+
+# 사용 예제
+linked_list = SinglyLinkedList()
+linked_list.append("A")
+linked_list.append("B")
+linked_list.append("C")
+linked_list.print_list()
