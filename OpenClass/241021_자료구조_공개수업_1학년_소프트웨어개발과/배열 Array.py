75ad79d9c4d5c8d7eb226d87a045f11d798e011b
은수 Eunsu <sublimage1109@gmail.com>
2024-10-23 10:09:11 +0900

    241021

---
+++ OpenClass/241021_자료구조_공개수업_1학년_소프트웨어개발과/배열 Array.py
@@ -0,0 +1,54 @@
+class Array:
+    def __init__(self, size):
+        self.size = size
+        self.array = [None] * size
+
+    def insert(self, index, value):
+        if 0 <= index < self.size:
+            self.array[index] = value
+
+    def delete(self, index):
+        if 0 <= index < self.size:
+            self.array[index] = None
+
+    def search(self, value):
+        for i in range(self.size):
+            if self.array[i] == value:
+                return i
+        return -1
+    
+size = int(input("Enter the size of the array: "))
+arr = Array(size)
+
+for i in range(size):
+    value = int(input(f"Enter value for index {i}: "))
+    arr.insert(i, value)
+
+while True:
+    action = input("Enter 'i' tp insert, 'd' to deleter, 's' to search, or 'q' to quit: ")
+
+    if action == 'i':
+        index = int(input("Enter the index to insert at: "))
+        value = int(input("Enter the value to insert: "))
+        arr.insert(index, value)
+        print(f"{value} has been inserted at index {index}.")
+
+    elif action == 'd':
+        index = int(input("Enter the index to delete:"))
+        arr.delete(index)
+        print(f"The value at index {index} has been deleted.")
+
+    elif action == 's':
+        value = int(input("Enter the value to search for: "))
+        index = arr.search(value)
+        if index != -1:
+            print(f"{value} is found at index {index}.")
+        else:
+            print(f"{value} is not in the array")
+
+    elif action == 'q':
+        print("Exiting the program.")
+        break
+
+    else:
+        print("Invalid input.")
