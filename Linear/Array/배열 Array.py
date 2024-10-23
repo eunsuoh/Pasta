5d2ee8d8b9e32f1cb3b935d51fdc126c9658ed72
은수 Eunsu <sublimage1109@gmail.com>
2024-10-23 09:59:17 +0900

    Update 배열 Array.py

--- Linear/Array/배열 Array.py
+++ Linear/Array/배열 Array.py
@@ -17,3 +17,34 @@ class Array:
                 return i
         return -1
     
+size = int(input("Enter the size of the array: "))
+arr = Array(size)
+
+while True:
+    action = input("Enter 'i' tp insert, 'd' to deleter, 's' to search, or 'q' to quit: ")
+
+    if action == 'i':
+        index = int(input("Enter the index to insert at: "))
+        value = int(input("Enter the value to insert: "))
+        arr.insert(index, value)
+        print(f"{value} has been inserted at index {index}.")
+
+    elif action == 'd':
+        index = int(input("Enter the index to delete:"))
+        arr.delete(index)
+        print(f"The value at index {index} has been deleted.")
+
+    elif action == 's':
+        value = int(input("Enter the value to search for: "))
+        index = arr.search(value)
+        if index != -1:
+            print(f"{value} is found at index {index}.")
+        else:
+            print(f"{value} is not in the array")
+
+    elif action == 'q':
+        print("Exiting the program.")
+        break
+
+    else:
+        print("Invalid input.")
